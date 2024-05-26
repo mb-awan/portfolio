ce8536efc5c3301735535605889532cedda033fa
mb-awan <bilalofficialmail123@gmail.com>
2024-05-26 18:03:59 +0500

    refactor: favicon updated

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,6 +14,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <link rel="shortcut icon" href="/favicon.png" type="image/x-icon" />
+      </head>
       <body>{children}</body>
     </html>
   );
