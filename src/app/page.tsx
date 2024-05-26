227fd98d904c367e100711efcad94d564ee0a36d
mb-awan <bilalofficialmail123@gmail.com>
2024-05-27 04:45:55 +0500

    feat: basic navbar is complete

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,9 @@
+import Navbar from "@/components/Navbar/Navbar";
+
 export default function Home() {
-  return <div>Home</div>;
+  return (
+    <div className="main-container">
+      <Navbar />
+    </div>
+  );
 }
