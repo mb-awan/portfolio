04ae6b1fa4c2aeaa12d435008ea1849d1ac14986
mb-awan <bilalofficialmail123@gmail.com>
2024-07-06 02:49:24 +0500

    feat: basic portfolio completed

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,22 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+import million from 'million/compiler';
 
-export default nextConfig;
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  experimental: {
+    optimizePackageImports: ['framer-motion'],
+    webVitalsAttribution: ['FCP', 'LCP', 'CLS', 'FID', 'TTFB', 'INP'],
+  },
+  images: {
+    deviceSizes: [390, 435, 768, 1024, 1280],
+    formats: ['image/avif'],
+    remotePatterns: [
+      { hostname: 'cdn.discordapp.com' },
+      { hostname: 'media.discordapp.net' },
+      { hostname: 'dcdn.dstn.to' },
+    ],
+  },
+  reactStrictMode: true,
+  trailingSlash: false,
+};
+
+export default million.next(nextConfig);
